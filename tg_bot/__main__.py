f6cd4fa4e50647e2defecf8571c4b6a3ef29e0ee
Dskunjava10 <90951752+Dskunjava10@users.noreply.github.com>
2021-10-20 12:15:05 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -150,7 +150,7 @@ def start(bot: Bot, update: Update, args: List[str]):
 
                 parse_mode=ParseMode.MARKDOWN, reply_markup=InlineKeyboardMarkup(
                     [[InlineKeyboardButton(text="🎉 Add me to your group 🎉", url="t.me/{}?startgroup=true".format(bot.username))],
-                     [InlineKeyboardButton(text="👨‍💻 Mʏ Dᴇᴠᴇʟᴏᴘᴇʀ ", url="https://t.me/DS_KUNJAVA"), InlinekeyboardButton(text="Sᴏᴜʀᴄᴇ Cᴏᴅᴇ", url="https://telegra.ph/file/a37f19af38c4a0c2f1f74.jpg")],
+                     [InlineKeyboardButton(text="👨‍💻 Mʏ Dᴇᴠᴇʟᴏᴘᴇʀ", url="https://t.me/DS_KUNJAVA"), InlinekeyboardButton(text="Sᴏᴜʀᴄᴇ Cᴏᴅᴇ", url="https://telegra.ph/file/a37f19af38c4a0c2f1f74.jpg")],
                      [InlineKeyboardButton(text="🔰 ɢʀᴏᴜᴘ 🔰", url="https://t.me/movie_house2"), InlineKeyboardButton(text="🌟 ᴄʜᴀɴɴᴇʟ 🌟", url="https://t.me/ds_movies1")],
                      [InlineKeyboardButton(text="💫 Help", url="https://t.me/{}?start=help".format(bot.username)) ]]))
 
